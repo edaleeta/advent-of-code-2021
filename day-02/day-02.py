da804f386629f4348e10373aa12d227ce7dbd67a
Edelita Valdez <edaleeta@gmail.com>
2021-12-03 22:49:58 -0800

    Completes Day 2, Part 2

--- day-02/day-02.py
+++ day-02/day-02.py
@@ -1,25 +1,39 @@
 
-def get_x_y_position():
-    x, y = 0, 0
+def get_x_y_position(mode=1):
+    x, y, aim = 0, 0, 0
     with open('day-02-input.txt') as input:
         for instruction in input:
-            direction, distance = instruction.split()
-            distance = int(distance)
+            direction, amount = instruction.split()
+            amount = int(amount)
 
             if direction == 'forward':
-                x += distance
+                x += amount
+                if mode != 1:
+                    y += aim * amount
+
             elif direction == 'down':
-                y += distance
+                if mode == 1:
+                    y += amount
+                else:
+                    aim += amount
+
             elif direction == 'up':
-                y -= distance
+                if mode == 1:
+                    y -= amount
+                else:
+                    aim -= amount
+
             else:
                 print(f'Unknown direction: {direction}')
 
     print(f"x: {x}, y: {y}")
     return x, y
 
-def get_position_multiplied():
-    x, y = get_x_y_position()
+
+def get_position_multiplied(mode=1):
+    x, y = get_x_y_position(mode)
     return x * y
 
-print(get_position_multiplied())
+
+print(get_position_multiplied(mode=1))
+print(get_position_multiplied(mode=2))
