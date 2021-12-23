abe05d26cb15bfbd418a6a5f6bd284b5dcda0225
Edelita Valdez <edaleeta@gmail.com>
2021-12-23 14:22:09 -0800

    Completes Day 10, Part 2

--- day-10/day-10.py
+++ day-10/day-10.py
@@ -8,11 +8,18 @@ OPEN_CHAR_TO_CLOSING_CHAR = {
     '<': '>',
 }
 
-CHAR_TO_SCORE = {
+CHAR_TO_ERROR_SCORE = {
     ')': 3,
     ']': 57,
     '}': 1197,
     '>': 25137,
+}
+
+CHAR_TO_AUTOCOMPLETE_SCORE = {
+    ')': 1,
+    ']': 2,
+    '}': 3,
+    '>': 4,
 }
 
 
@@ -28,19 +35,52 @@ def get_first_invalid_char(chars):
     return ''
 
 
-def parse_input():
-    chars = "{([(<{}[<>[]}>{[]{[(<()>"
-    return [char for char in chars]
-
-
 def get_syntax_error_score():
     error_score = 0
     with open(FILE_NAME) as input:
         for line in input:
-            chars = [char for char in line]
+            chars = [char for char in line.strip()]
             invalid_char = get_first_invalid_char(chars)
-            error_score += CHAR_TO_SCORE.get(invalid_char, 0)
+            error_score += CHAR_TO_ERROR_SCORE.get(invalid_char, 0)
     return error_score
 
 
+def get_completing_sequence(chars):
+    open_chars = []
+    for char in chars:
+        if char in OPEN_CHAR_TO_CLOSING_CHAR:
+            open_chars.append(char)
+        elif OPEN_CHAR_TO_CLOSING_CHAR.get(open_chars[-1]) == char:
+            open_chars.pop()
+        else:
+            # corrupt sequence
+            return []
+
+    if len(open_chars) == 0:
+        # complete sequence
+        return []
+
+    open_chars.reverse()
+    return [OPEN_CHAR_TO_CLOSING_CHAR.get(char) for char in open_chars]
+
+
+def get_middle_autocomplete_score():
+    autocomplete_scores = []
+    with open(FILE_NAME) as input:
+        for line in input:
+            chars = [char for char in line.strip()]
+            completing_sequence = get_completing_sequence(chars)
+            if completing_sequence:
+                score = 0
+                for char in completing_sequence:
+                    score *= 5
+                    score += CHAR_TO_AUTOCOMPLETE_SCORE.get(char)
+                autocomplete_scores.append(score)
+
+    autocomplete_scores.sort()
+    mid = len(autocomplete_scores) // 2
+    return autocomplete_scores[mid]
+
+
 print(get_syntax_error_score())
+print(get_middle_autocomplete_score())
